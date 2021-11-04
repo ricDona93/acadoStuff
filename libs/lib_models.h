b65ebccae65ee90002a3016e05173245bb53b0de
ricDona93 <riccardo.dona@unitn.it>
2021-11-04 15:15:44 +0100

    added extenal initializiation variabiles

--- libs/lib_models.h
+++ libs/lib_models.h
@@ -5,9 +5,25 @@
 #ifndef TESTACADO_LIB_MODELS_H
 #define TESTACADO_LIB_MODELS_H
 
+// global variables
+extern const double KKT_th;
+
+extern const double betaMax;
+extern const double deltaMax;
+extern const double ayMax;
+extern const double jMax;
+
+extern const double lf;
+extern const double lr ;
+extern const double Kr;
+extern const double Kf;
+extern const double  m;
+extern const double  I;
+
+
 double calcTimeST_delta(double V, double nF);
 
-//double calcTimeST_pac(double V, double nF);
+double calcTimeST_pac(double V, double nF);
 
 double calcTimeST_jerk(double V, double nF);
 
